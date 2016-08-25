c4c7717538210718a17aaaf023d9a5d61ce3288b
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-26 01:38:23 +0300

    Prometheus.Contrib.HTTP tests

---
+++ test/contrib/http_test.exs
@@ -0,0 +1,21 @@
+defmodule PrometheusEx.Contrib.HTTPTest do
+  use ExUnit.Case
+  ## alias Prometheus.Error %% FIXME: status_class should throw invalid_value.
+  require Prometheus.Contrib.HTTP
+
+  test "microseconds_duration_buckets" do
+    assert [10, 100, 1000, 10000, 100000, 300000, 500000,
+            750000, 1000000, 1500000, 2000000, 3000000] == Prometheus.Contrib.HTTP.microseconds_duration_buckets
+  end
+
+  test "status_class" do
+    assert 'unknown' == Prometheus.Contrib.HTTP.status_class(50)
+    assert 'informational' == Prometheus.Contrib.HTTP.status_class(150)
+    assert 'success' == Prometheus.Contrib.HTTP.status_class(250)
+    assert 'redirection' == Prometheus.Contrib.HTTP.status_class(350)
+    assert 'client-error' == Prometheus.Contrib.HTTP.status_class(450)
+    assert 'server-error' == Prometheus.Contrib.HTTP.status_class(550)
+    assert 'unknown' == Prometheus.Contrib.HTTP.status_class(650)
+  end
+
+end
