4123e4bd1cd1c74bbd4f5b73759abf7bc575272f
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-26 01:27:06 +0300

    Prometheus.Buckets tests

--- test/buckets_test.exs
+++ test/buckets_test.exs
@@ -2,11 +2,31 @@ defmodule PrometheusEx.BucketsTest do
   use ExUnit.Case
   alias Prometheus.Error
   require Prometheus.Buckets
-  
 
-  test "linear buckets generator errors" do
+
+  test "linear buckets generator tests" do
     assert_raise Error.InvalidValue, fn ->
       Prometheus.Buckets.linear(-15, 5, 0)
     end
+
+    assert [-15, -10, -5, 0, 5, 10] == Prometheus.Buckets.linear(-15, 5, 6)
+  end
+
+  test "exponential buckets generator tests" do
+    assert_raise Error.InvalidValue, fn ->
+      Prometheus.Buckets.exponential(-15, 5, 0)
+    end
+    assert_raise Error.InvalidValue, fn ->
+      Prometheus.Buckets.exponential(-15, 5, 2)
+    end
+    assert_raise Error.InvalidValue, fn ->
+      Prometheus.Buckets.exponential(15, 0.5, 3)
+    end
+
+    assert [100, 120, 144] == Prometheus.Buckets.exponential(100, 1.2, 3)
+  end
+
+  test "default buckets test" do
+    assert [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10] == Prometheus.Buckets.default
   end
 end
