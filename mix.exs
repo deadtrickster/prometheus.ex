c13f5fa9f58fb46e4601100b334c8921600148d5
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-26 12:47:04 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrometheusEx.Mixfile do
 
   def project do
     [app: :prometheus_ex,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
