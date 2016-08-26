c28e8485087f4767782aca0744e56bed275dd8db
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-26 12:46:31 +0300

    update prometheus to 2.2.0

--- mix.exs
+++ mix.exs
@@ -12,11 +12,9 @@ defmodule PrometheusEx.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :prometheus]]
+    [applications: [:logger,
+                    :prometheus]]
   end
 
   defp description do
@@ -30,23 +28,13 @@ defmodule PrometheusEx.Mixfile do
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/deadtrickster/prometheus.ex",
               "Prometheus.erl" => "https://hex.pm/packages/prometheus",
-              "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
               "Ecto Instrumenter" => "https://hex.pm/packages/prometheus_ecto",
               "Phoenix Instrumenter" => "https://hex.pm/packages/prometheus_phoenix",
+              "Plugs Instrumenter/Exporter" => "https://hex.pm/packages/prometheus_plugs",
               "Process info Collector" => "https://hex.pm/packages/prometheus_process_collector"}]
   end
 
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:prometheus, "~> 2.0"}]
+    [{:prometheus, "~> 2.2.0"}]
   end
 end
