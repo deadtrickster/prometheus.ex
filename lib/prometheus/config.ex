cde2520579ac98daeba64d69f9f1ff19ebac6833
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-28 23:00:13 +0300

    mandatory options support

--- lib/prometheus/config.ex
+++ lib/prometheus/config.ex
@@ -1,25 +1,60 @@
 defmodule Prometheus.Config do
+
+  defmodule KeyNotFoundError do
+    defexception [:option, :key]
+
+    def message(%{option: option, key: key}) do
+      friendly_key_name = String.replace_leading("#{key}", "Elixir.", "")
+      "mandatory option :#{option} not found in #{friendly_key_name} instrumenter/collector config"
+    end
+  end
+
   defmacro __using__(default_config) do
+
+    keyword_default_config = Enum.reject(default_config, fn(option) ->
+      case option do
+        {_, _} -> false
+        _ -> true
+      end
+    end)
 
     quote do
       defmodule Config do
         @moduledoc false
 
         def config(key) do
-          Application.get_env(:prometheus, key, unquote(default_config))
+          Application.get_env(:prometheus, key, unquote(keyword_default_config))
         end
 
-        defp config(key, name, default) do
+        defp config(key, option, default) do
           config(key)
-          |> Keyword.get(name, default)
+          |> Keyword.get(option, default)
+        end
+
+        defp config(key, option) do
+          try do
+            config(key)
+            |> Keyword.fetch!(option)
+          rescue
+            e in KeyError -> raise %KeyNotFoundError{key: key, option: option}
+          end
         end
 
         unquote do
-          for {option, default} <- default_config do
-            quote do
-              def unquote(option)(key) do
-                config(key, unquote(option), unquote(default))
-              end
+          for config <- default_config do
+            case config do
+              {option, default} ->
+                quote do
+                  def unquote(option)(key) do
+                    config(key, unquote(option), unquote(default))
+                  end
+                end
+              option ->
+                quote do
+                  def unquote(:"#{option}!")(key) do
+                    config(key, unquote(option))
+                  end
+                end
             end
           end
         end
