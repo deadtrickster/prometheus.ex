80c053136886d74bc39b629c099d46275e8900fe
Ilya Khaprov <dead.trickster@gmail.com>
2016-09-04 00:47:53 +0300

    cleanup docs

--- lib/prometheus.ex
+++ lib/prometheus.ex
@@ -41,14 +41,14 @@ defmodule Prometheus do
 
   ### Standard Metrics & Registry
 
-   - [`Prometheus.Metric.Counter`](Prometheus.Metric.Counter.html) - counter metric, to track counts of events or running totals;
-   - [`Prometheus.Metric.Gauge`](Prometheus.Metric.Gauge.html) - histogram metric, to track distributions of events;
-   - [`Prometheus.Metric.Histogram`](Prometheus.Metric.Histogram.html) - gauge metric, to report instantaneous values;
-   - [`Prometheus.Metric.Summary`](Prometheus.Metric.Summary.html) - summary metric, to track the size of events;
-   - [`Prometheus.Registry`](Prometheus.Registry.html) - working with Prometheus registries.
+   - `Prometheus.Metric.Counter` - counter metric, to track counts of events or running totals;
+   - `Prometheus.Metric.Gauge` - histogram metric, to track distributions of events;
+   - `Prometheus.Metric.Histogram` - gauge metric, to report instantaneous values;
+   - `Prometheus.Metric.Summary` - summary metric, to track the size of events;
+   - `Prometheus.Registry` - working with Prometheus registries.
 
-  All metrics created via `new/1` or `declare/1` macros. The difference is `new/1` actually wants metric to be
-  new and raises [`Prometheus.Error.MFAlreadyExists`](Prometheus.Error.MFAlreadyExists.html) if it isn't.
+  All metrics created via `new/1` or `declare/1` macros. The difference is that `new/1` actually wants metric to be
+  new and raises `Prometheus.MFAlreadyExistsError` if it isn't.
 
   Both `new/1` and `declare/1` accept options as [Keyword](http://elixir-lang.org/docs/stable/elixir/Keyword.html).
   Common options are:
@@ -62,25 +62,25 @@ defmodule Prometheus do
 
   ### General Helpers
 
-   - [`Prometheus.Buckets`](Prometheus.Buckets.html) - linear or exponential bucket generators;
-   - [`Prometheus.Contrib.HTTP`](Prometheus.Contrib.HTTP.html) - helpers for HTTP instrumenters.
+   - `Prometheus.Buckets` - linear or exponential bucket generators;
+   - `Prometheus.Contrib.HTTP` - helpers for HTTP instrumenters.
 
   ### Integration Helpers
 
-   - [`Prometheus.Config`](Prometheus.Config.html) - provides standard configuration mechanism for custom instrumenters/exporters.
+   - `Prometheus.Config` - provides standard configuration mechanism for custom instrumenters/exporters.
 
 
   ### Exposition Formats
 
-   - [`Prometheus.Format.Text`](Prometheus.Format.Text.html) - renders metrics for a given registry (default is `:default`) in text format;
-   - [`Prometheus.Format.Protobuf`](Prometheus.Format.Protobuf.text) - renders metrics for a given registry (default is `:default`) in protobuf v2 format.
+   - `Prometheus.Format.Text` - renders metrics for a given registry (default is `:default`) in text format;
+   - `Prometheus.Format.Protobuf` - renders metrics for a given registry (default is `:default`) in protobuf v2 format.
 
   ### Advanced
 
   You will need this modules only if you're writing custom collector for app/lib that can't be instrumented directly.
 
-   - [`Prometheus.Collector`](Prometheus.Collector.html) - exports macros for managing/creating collectors;
-   - [`Prometheus.Model`](Prometheus.Model.html) - provides API for working with underlying Prometheus models.
+   - `Prometheus.Collector` - exports macros for managing/creating collectors;
+   - `Prometheus.Model` - provides API for working with underlying Prometheus models.
    You'll use that if you want to create custom collector.
 
 
@@ -92,7 +92,6 @@ defmodule Prometheus do
       require Prometheus.Collector
       require Prometheus.Registry
       require Prometheus.Buckets
-      require Prometheus.Error
       use Prometheus.Metric
       require Prometheus.Contrib.HTTP
     end
